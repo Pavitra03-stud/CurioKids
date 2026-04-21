0ef9a7b485b416d45544ef9ef216fd2add97fb9d
unknown <Pavitragv24cs@srishakthi.ac.in>
2026-04-21 09:03:43 +0530

    New changes

--- src/features/GamesHome.jsx
+++ src/features/GamesHome.jsx
@@ -1,33 +1,25 @@
 import "../styles/GamesHome.css";
-import BackIcon from "../components/BackIcon";
+import { useNavigate } from "react-router-dom";
 
-export default function GamesHome({ navigate, goBack }) {
+export default function GamesHome() {
+  const navigate = useNavigate();
+
   return (
     <div className="games-page">
-      <div className="floating-bg bubble1"></div>
-      <div className="floating-bg bubble2"></div>
-      <div className="floating-bg bubble3"></div>
 
       <header className="games-topbar">
-        <button className="games-back" onClick={goBack}>
-          ←
-        </button>
         <h1 className="games-topbar-title">🎮 Games</h1>
       </header>
 
       <div className="games-header">
-        <div className="header-mascots">
-          <span>🐯</span>
-          <span>🦊</span>
-          <span>🐻</span>
-        </div>
         <p>Choose a zone and start playing</p>
       </div>
 
       <div className="games-container">
+
         <div
           className="games-card learning"
-          onClick={() => navigate("games-learning-home")}
+          onClick={() => navigate("/games-learning")}
           style={{ cursor: "pointer" }}
         >
           📘 Learning Zone
@@ -35,18 +27,14 @@ export default function GamesHome({ navigate, goBack }) {
 
         <div
           className="games-card play"
-          onClick={() => navigate("games-play-home")}
+          onClick={() => navigate("/games-play")}
           style={{ cursor: "pointer" }}
         >
           🎮 Game Zone
         </div>
+
       </div>
 
-      <div className="bottom-animals">
-        <span>🦁</span>
-        <span>🐼</span>
-        <span>🐵</span>
-      </div>
     </div>
   );
 }
